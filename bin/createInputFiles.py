c8a5b071b5dda33c4adb5c30b5f276c7a2277184
Sharon Giannatto <Sharon.Giannatto@jax.org>
2010-01-05 13:39:35 +0000

    proload-4-3-3-1

---
+++ bin/createInputFiles.py
@@ -0,0 +1,121 @@
+#!/usr/local/bin/python
+
+##########################################################################
+#
+# Purpose:
+#       From PRO input file create vocload and assocload input file
+#
+# Usage: createInputFiles.py
+# Env Vars:
+#	 1. OUTFILE_NAME
+#
+# Inputs:
+#	1. PRO ID to MGI mapping file tab-delimited in following format:
+#	    1. PRO ID
+#	    2. PRO name
+#	    3. MGI ID(s) ';' (semi-colon) delimited
+#	2. Configuration (see proload.config, vocload.config)
+#
+# Outputs:
+#	 1. tab delimited file vocload format:
+#           1. term
+#           2. accession id
+#           3. blank
+#           4. blank
+#           5. blank
+#           6. blank
+#           7. blank
+#           8. blank
+#	 2. tab delimited file in assocload format:
+#	    line 1 header: "MGI" "Protein Ontology"
+# 	    1. MGI ID
+#           2. PRO ID
+#	 3. log file
+# 
+# Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#
+#  Assumes:  Nothing
+#
+#  Notes:  None
+#
+###########################################################################
+
+import sys
+import os
+import mgi_utils
+import loadlib
+import string
+
+print '%s' % mgi_utils.date()
+
+# paths to input and two output files
+inFilePath = os.environ['INFILE_NAME_PRO']
+vocFilePath = os.environ['INFILE_NAME_VOC']
+assocFilePath= os.environ['INFILE_NAME']
+
+# file descriptors
+
+# input
+inFile = ''
+# output for vocload
+vocFile = ''
+# output for assocload
+assocFile = ''
+
+TAB= '\t'
+CRT = '\n'
+SPACE = ' '
+
+#
+# Initialize
+#
+
+try:
+    inFile = open(inFilePath, 'r')
+except:
+    exit('Could not open file for reading %s\n' % inFilePath)
+
+try:
+    vocFile = open(vocFilePath, 'w')
+except:
+    exit('Could not open file for writing %s\n' % vocFilePath)
+
+try:
+    assocFile = open(assocFilePath, 'w')
+except:
+    exit('Could not open file for writing %s\n' % assocFilePath)
+
+#
+# Process
+#
+
+# write out assocload header
+assocFile.write('%s%s%s%s' % ('MGI', TAB, 'PRO', CRT))
+
+# throw away header line
+header = inFile.readline()
+
+for line in inFile.readlines():
+    (proId, proName, mgiId) = string.split(line, TAB)
+    proId = string.strip(proId)
+    proName = string.strip(proName)
+    mgiIds = string.strip(mgiId)
+    #mgiId = "MGI:%s" % mgiId
+    
+    vocFile.write('%s%s%s%s%s%s%s%s%s%s' % (proName, TAB, proId, TAB, TAB, TAB, TAB, TAB, TAB, CRT))
+    for m in string.split(mgiIds, ';'):
+        mgiId = string.strip(m)
+	assocFile.write('%s%s%s%s' % (mgiId, TAB, proId, CRT))
+    
+#
+# Post Process
+#
+
+inFile.close()
+vocFile.close()
+assocFile.close()
+
+print '%s' % mgi_utils.date()
---
+++ bin/proload.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+#
+#  proload.sh
+###########################################################################
+#
+#  Purpose:
+# 	This script creates Protein Ontology vocload and assocload
+#       input file and invokes vocload and assocload
+#
+  Usage=proload.sh
+#
+#  Env Vars:
+#
+#      See the configuration file
+#
+#  Inputs:
+#
+#      - Common configuration file -
+#               /usr/local/mgi/live/mgiconfig/master.config.sh
+#      - PRO load configuration file - proload.config
+#      - input file - see python script header
+#
+#
+#  Outputs:
+#
+#      - An archive file
+#      - Log files defined by the environment variables ${LOG_PROC},
+#        ${LOG_DIAG}, ${LOG_CUR} and ${LOG_VAL}
+#      - Input files for vocload and assocload
+#      - see vocload and assocload outputs
+#      - Records written to the database tables
+#      - Exceptions written to standard error
+#      - Configuration and initialization errors are written to a log file
+#        for the shell script
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal error occurred
+#      2:  Non-fatal error occurred
+#
+#  Assumes:  Nothing
+#
+# History:
+#
+# sc	12/18/2009 - created
+#
+
+cd `dirname $0`
+LOG=`pwd`/proload.log
+rm -rf ${LOG}
+
+CONFIG_LOAD=../proload.config
+
+#
+# verify & source the configuration file
+#
+
+if [ ! -r ${CONFIG_LOAD} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+. ${CONFIG_LOAD}
+
+#
+#  Source the DLA library functions.
+#
+
+if [ "${DLAJOBSTREAMFUNC}" != "" ]
+then
+    if [ -r ${DLAJOBSTREAMFUNC} ]
+    then
+        . ${DLAJOBSTREAMFUNC}
+    else
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${LOG}
+        exit 1
+    fi
+else
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${LOG}
+    exit 1
+fi
+
+#####################################
+#
+# Main
+#
+#####################################
+
+#
+# createArchive including OUTPUTDIR, startLog, getConfigEnv
+# sets "JOBKEY"
+preload ${OUTPUTDIR}
+
+#
+# create input files
+#
+echo 'Running createInputFiles.py' >> ${LOG_DIAG}
+${PROLOAD}/bin/createInputFiles.py
+STAT=$?
+checkStatus ${STAT} "${PROLOAD}/bin/createInputFiles.py"
+
+#
+# run the vocabulary load
+#
+echo "Running vocload to load Protein Ontology" >> ${LOG_DIAG}
+${VOCLOAD}/runSimpleFullLoad.sh ${PROLOAD}/vocload.config
+STAT=$?
+checkStatus ${STAT} "${VOCLOAD}/runSimpleFullLoad.sh ${PROLOAD}/vocload.config"
+
+#
+# run association load
+#
+
+# set to full path for assocload
+CONFIG_LOAD=${PROLOAD}/proload.config
+
+echo "Running Protein Ontology association load" >> ${LOG_DIAG}
+${ASSOCLOADER_SH} ${CONFIG_LOAD} ${JOBKEY}
+STAT=$?
+checkStatus ${STAT} "${ASSOCLOADER_SH}/bin/AssocLoad.sh ${CONFIG_LOAD}"
+
+#
+# run postload cleanup and email logs
+#
+shutDown
